646048ce1e6e765f628b21eef69e7d12b2275009
Khalid Qarryzada <khalid.qarryzada@utexas.edu>
2018-10-16 22:17:06 -0500

    Collaborated on skeleton of file

---
+++ main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    public void print() {
+
+    }
+
+    public void scramble() {
+
+    }
+
+    public boolean check() {
+
+    }
+
+    public void getInput() {
+        
+    }
+}
